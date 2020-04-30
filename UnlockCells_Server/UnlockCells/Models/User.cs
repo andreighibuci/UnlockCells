bcfd5990b562adf47f7ab18c491d4d3c21b6e766
Maneadragos <32910098+Maneadragos@users.noreply.github.com>
2020-04-30 23:17:35 +0300

    Added Models and controller for User Manage

---
+++ UnlockCells_Server/UnlockCells/Models/User.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UnlockCells.Models
+{
+    public class User
+    {
+        [Key]
+        public int UserId { get; set; }
+        [Column(TypeName = "nvarchar(250)")]
+        [Required(ErrorMessage = "This field is required.")]
+        [DisplayName("Full Name")]
+        public string FullName { get; set; }
+        [Column(TypeName = "varchar(10)")]
+        [DisplayName("RFID Code")]
+        public string RFIDCode { get; set; }
+        [Column(TypeName = "varchar(1)")]
+        public int Access { get; set; }
+        [Column(TypeName = "varchar(100)")]
+        [DisplayName("Department")]
+        public string Department { get; set; }
+    }
+}
---
+++ UnlockCells_Server/UnlockCells/Models/UserContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UnlockCells.Models
+{
+    public class UserContext : DbContext
+    {
+        public UserContext(DbContextOptions<UserContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
